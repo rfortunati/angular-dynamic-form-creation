257e32b1875c6263e7ac56ad201a630fe85642ed
Richard Fortunati <r.fortunati@risa-it.nl>
2023-08-01 07:51:11 +0200

    no message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 import { QuestionService } from './question.service';
 import { QuestionBase } from './question-base';
-import { Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { DocumentProperties } from './document.properties';
 
 @Component({
@@ -13,22 +13,28 @@ import { DocumentProperties } from './document.properties';
       <app-dynamic-form [questions]="questions$ | async"></app-dynamic-form>
     </div>
 
+<!--    <div *ngFor="let d of documentProperties$ | async">-->
+<!--      <p>{{d.name}}, {{d.objectType}}, {{d.isEditable}}</p>-->
+<!--    </div>-->
+
     <h1>My Custom Form</h1>
-    <div *ngFor="let d of documentProperties$ | async">
-      <p>{{d.name}}, {{d.objectType}}, {{d.isEditable}}</p>
+    <div>
+      <app-dynamic-form [questions]="test$ | async"></app-dynamic-form>
     </div>
+    
   `,
   providers: [QuestionService],
 })
 export class AppComponent {
   questions$: Observable<QuestionBase<any>[]>;
-  documentProperties$: Observable<DocumentProperties[]>;
+  // documentProperties$: Observable<DocumentProperties[]>;
+  test$: Observable<QuestionBase<any>[]>
 
   constructor(service: QuestionService) {
     this.questions$ = service.getQuestions();
-    this.documentProperties$ = service.getDocumentProperties();
-
-    service.getTest().subscribe((a) => console.log(a));
+    // this.documentProperties$ = service.getDocumentProperties();
+    this.test$ = service.getTest();
+    // service.getTest().subscribe((a) => console.log(a));
   }
 }
 
--- src/app/document.properties.ts
+++ src/app/document.properties.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Adapter } from './adapter';
 import { QuestionBase } from './question-base';
+import { DropdownQuestion } from './question-dropdown'
+import { TextboxQuestion } from './question-textbox'
 
 export interface DocumentProperties {
   name: string;
@@ -10,22 +12,38 @@ export interface DocumentProperties {
   options?: string[];
 }
 
+
+export interface Options {
+  key: string;
+  value: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
-export class DocumentPropetiesAdapter implements Adapter<QuestionBase<string>> {
+export class DocumentPropertiesAdapter implements Adapter<QuestionBase<string>> {
   adapt(d: DocumentProperties): QuestionBase<string> {
     let q: QuestionBase<string> = {
       value: d.value,
       key: d.name.toLowerCase().replace(/\s/g, ''),
       label: d.name,
-      required: false,
+      required: true,
       // order: 1,
       controlType: d.objectType,
       type: d.objectType,
       // for options we need to create key value pair
       // options: d.options
     };
-    return q;
+
+    if (d.objectType === 'options' && !!d.options) {
+      return new DropdownQuestion({
+        ...q,
+        options: [{key: 'a', value: 'a'}]
+      })
+    } else {
+      return new TextboxQuestion({
+        ...q
+      })
+    }
   }
 }
